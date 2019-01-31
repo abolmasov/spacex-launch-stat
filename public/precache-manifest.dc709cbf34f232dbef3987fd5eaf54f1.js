d65aee4027ff80f0a13690d9eb4ec158bed12d9f
Roman <sillencium@gmail.com>
2019-01-31 18:39:45 +0200

    'Deploy'

---
+++ public/precache-manifest.dc709cbf34f232dbef3987fd5eaf54f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd9ccb35bdd88e54a4d0",
+    "url": "/static/js/main.bd9ccb35.chunk.js"
+  },
+  {
+    "revision": "9c5b2409d81824791960",
+    "url": "/static/js/1.9c5b2409.chunk.js"
+  },
+  {
+    "revision": "bd9ccb35bdd88e54a4d0",
+    "url": "/static/css/main.60e0d0e5.chunk.css"
+  },
+  {
+    "revision": "35edf40c91bd20a25d946745dd2d064d",
+    "url": "/index.html"
+  }
+];
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema");
+const cors = require("cors");
+const app = express();
+const path = requier("path");
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => console.log(`Server started at ${PORT}`));
