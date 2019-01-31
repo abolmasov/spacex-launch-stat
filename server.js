1e54fc4cfd5b7f32072c318144ebcc4d32f66117
Roman <sillencium@gmail.com>
2019-01-31 18:43:05 +0200

    'Update-issue'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const cors = require("cors");
 const app = express();
-const path = requier("path");
+const path = require("path");
 
 app.use(cors());
 
